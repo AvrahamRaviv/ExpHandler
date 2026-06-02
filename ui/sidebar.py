709ee70e647119c2796e1a23a34850250eb1071e
avrahamraviv <avrahamsapir1@gmail.com>
2026-06-02 14:51:14 +0300

    Bump version to v0.2.7

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -32,7 +32,7 @@ class Sidebar(QWidget):
         self.title.setAlignment(Qt.AlignCenter)
         layout.addWidget(self.title)
 
-        self.version = QLabel("v0.2.6")
+        self.version = QLabel("v0.2.7")
         self.version.setAlignment(Qt.AlignCenter)
         layout.addWidget(self.version)
 
